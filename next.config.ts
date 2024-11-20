609c85ab3ecae24fd1cb0afb5c64feac9885ec48
Arnav Bharti <arnavbharti@icloud.com>
2024-11-19 20:59:07 -0800

    removed public files and dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+import bundleAnalyzer from '@next/bundle-analyzer';
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer(nextConfig)
 
 export default nextConfig;
